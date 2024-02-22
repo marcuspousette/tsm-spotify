f5af4e6dadfcfca458f240399c84962db8c99e3c
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-22 04:29:12 +0100

    Code was missing

--- src/theme/material-theme.js
+++ src/theme/material-theme.js
@@ -2,12 +2,20 @@ import { createTheme } from '@mui/material';
 
 export const themeOptions = createTheme({
   palette: {
-    primary: { main: '#1bd760', light: '#39d472', dark: '#3b5249' },
+    primary: {
+      main: '#1bd760',
+      light: '#39d472',
+      dark: '#3b5249',
+      contrastText: '#ffffff'
+    },
     background: {
-      default: '#000',
+      default: '#000000',
       paper: '#121212'
     },
-    text: { primary: '#fff', secondary: '#b3b3b3' },
+    text: {
+      primary: '#ffffff',
+      secondary: '#b3b3b3'
+    },
     divider: '#292929'
   }
 });
