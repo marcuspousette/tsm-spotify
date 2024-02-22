c69d52a66bd22c19490e8247587673c30d33e694
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-22 06:04:58 +0100

    Conditional rendering for Dashboard/Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import './App.css';
 import { Box } from '@mui/material';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './pages/Login';
+import { getAccessToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App({ spotifyApi }) {
+  const [token, setToken] = useState(getAccessTokenFromStorage());
+
+  useEffect(() => {
+    const accessToken = getAccessTokenFromStorage() || getAccessToken();
+    if (accessToken) {
+      setToken(accessToken);
+      sessionStorage.setItem('spotifyToken', accessToken);
+      window.location.hash = '';
+    }
+  }, []);
+
   return (
     <Box className="App">
-      <Login />
-      <Dashboard spotifyApi={spotifyApi} />
+      {token ? (
+        <Dashboard spotifyApi={spotifyApi} />
+      ) : (
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )}
     </Box>
   );
 }
