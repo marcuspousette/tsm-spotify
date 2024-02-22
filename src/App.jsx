2c19e10d8a8ea7a00fdfc0a52ee8d2c4ede6d819
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-22 05:40:16 +0100

    Lektion 8: Access Token

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Box } from '@mui/material';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './pages/Login';
 
 function App({ spotifyApi }) {
   return (
     <Box className="App">
+      <Login />
       <Dashboard spotifyApi={spotifyApi} />
     </Box>
   );
