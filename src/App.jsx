7806ed6654a73685dd10954de73c90bea23c6417
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-22 04:57:49 +0100

    Lektion 7: Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
 import { Box } from '@mui/material';
-import Login from './pages/Login';
-import Home from './pages/Home';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App({ spotifyApi }) {
   return (
     <Box className="App">
-      <Home />
+      <Dashboard spotifyApi={spotifyApi} />
     </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import SpotifyWebApi from 'spotify-web-api-node';
 import { redirectURL } from './config/config';
 import { ThemeProvider } from '@mui/system';
 import { themeOptions } from './theme/material-theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const spotifyApi = new SpotifyWebApi({
   clientId: import.meta.env.VITE_CLIENT_ID,
