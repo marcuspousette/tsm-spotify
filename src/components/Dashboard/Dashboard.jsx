fea517bd2cdb69d456f1f6d4babcdb53a56c9991
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-23 08:27:50 +0100

    Fixed token in dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
+import { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
 import SideNav from '../SideNav/SideNav';
 import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
-import { useState, useEffect } from 'react';
 
 const Dashboard = ({ spotifyApi }) => {
   const [token, setToken] = useState(getAccessTokenFromStorage);
@@ -19,7 +19,7 @@ const Dashboard = ({ spotifyApi }) => {
   return (
     <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
       <Box sx={{ flex: 1, overflowY: 'auto', display: 'flex' }}>
-        <SideNav spotifyApi={spotifyApi} token={null} />
+        <SideNav spotifyApi={spotifyApi} token={token} />
         <Routes>
           <Route path="/playlist/:id" element={<div>Playlist</div>} />
           <Route path="/library" element={<div>Library</div>} />
