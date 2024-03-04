d3592ce67d8bca6f2a845ba700892326b25448e9
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-03-05 00:43:57 +0100

    Player styling

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,9 +3,23 @@ import { Avatar, Box, Grid, Typography } from '@mui/material';
 const Player = ({ spotifyApi }) => {
   return (
     <Box>
-      <Grid container>
+      <Grid
+        container
+        px={3}
+        sx={{
+          backgroundColor: 'background.paper',
+          height: 100,
+          cursor: { xs: 'pointer', md: 'auto' },
+          width: '100%',
+          borderTop: '1px solid #292929'
+        }}
+      >
         <Grid item xs={12} md={4} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-          Image, Title, Artist
+          <Avatar src={null} alt={null} variant="square" sx={{ width: 56, height: 56, marginRight: 2 }} />
+          <Box>
+            <Typography sx={{ color: 'text.primary', fontSize: 14 }}>Title</Typography>
+            <Typography sx={{ color: 'text.secondary', fontSize: 12 }}>Artist</Typography>
+          </Box>
         </Grid>
         <Grid
           item
