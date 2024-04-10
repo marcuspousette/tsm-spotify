8b6bf28dc03498c1557a0c04fb6d4fb14bdd8659
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-04-10 06:12:06 +0200

    SkipPrevious, Play/Pause and SkipNext buttons added

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -4,9 +4,21 @@ import { Pause, PlayArrow, SkipNext, SkipPrevious } from '@mui/icons-material';
 import { useState } from 'react';
 
 const PlayerControls = ({ player, isPaused, duration, progress }) => {
+  const skipStyle = { width: 28, height: 28 };
+  const playStyle = { width: 38, height: 38 };
   return (
-    <Stack direction={'column'} spacing={2} justify="center" alignItems="center" sx={{ width: '100%' }}>
-      <Stack>Play knapparna</Stack>
+    <Stack direction="column" spacing={2} justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+      <Stack direction="row" spacing={1} justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+        <IconButton size="small" sx={{ color: 'text.primary' }}>
+          <SkipPrevious sx={skipStyle} />
+        </IconButton>
+        <IconButton size="small" sx={{ color: 'text.primary' }}>
+          {isPaused ? <PlayArrow sx={playStyle} /> : <Pause sx={playStyle} />}
+        </IconButton>
+        <IconButton size="small" sx={{ color: 'text.primary' }}>
+          <SkipNext sx={skipStyle} />
+        </IconButton>
+      </Stack>
       <Stack>Progress/Slider</Stack>
     </Stack>
   );
