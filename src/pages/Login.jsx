8e3c8347e0671f14a0d33729636820889d10a88e
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-04-24 08:21:58 +0200

    Revert "Illegal redirect test fix"
    This reverts commit 6edd757226a221f561cef19ae72d7babe338a631.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,7 @@
 import { Box, Button } from '@mui/material';
-import { accessURL, authEndpoint, clientId, liveURL, devURL, scopes } from '../config/config';
+import { accessURL } from '../config/config';
 
 const Login = () => {
-  const accessUrl1 = `${authEndpoint}?client_id=${clientId}&redirect_uri=${
-    import.meta.env.PROD ? liveURL : devURL
-  }&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-  const accessUrl2 = `${authEndpoint}?client_id=${clientId}&redirect_uri=${
-    import.meta.env.MODE === 'production' ? liveURL : devURL
-  }&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-
   return (
     <Box
       sx={{
@@ -21,17 +14,9 @@ const Login = () => {
       }}
     >
       <img src="/Spotify_Logo.png" alt="Spotify Logo" style={{ marginBottom: 300, width: '70%', maxWidth: 500 }} />
-      <Box sx={{ display: 'flex', gap: 4 }}>
-        <Button color="primary" size="large" variant="contained" href={accessURL}>
-          Login to Spotify (Old)
-        </Button>
-        <Button color="primary" size="large" variant="contained" href={accessUrl1}>
-          Login to Spotify (Test 1)
-        </Button>
-        <Button color="primary" size="large" variant="contained" href={accessUrl2}>
-          Login to Spotify (Test 2)
-        </Button>
-      </Box>
+      <Button color="primary" size="large" variant="contained" href={accessURL}>
+        Login to Spotify
+      </Button>
     </Box>
   );
 };
